75bf2b0b5c3a91cf61faff64204508731dbc1090
spatulatom <spatulatom@gmail.com>
2021-04-10 13:30:39 +0100

    gh-pages

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -66,7 +66,7 @@ class App extends React.Component{
   render(){
     return(
       <div>
-  <h1>St<span className="highlight"> Listęe Spotyfajowych</span> Kawałków</h1>
+  <h1>Wykreuj<span className="highlight"> Listęe Spotyfajowych</span> Kawałków</h1>
   <div className="App">
     <SearchBar onSearch ={this.search}/>
     <div className="App-playlist">
