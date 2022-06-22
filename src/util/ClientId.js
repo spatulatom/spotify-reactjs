4bf99240f6e0c1931bdcbe71684a0dc0ae7b8ecf
spatulatom <spatulatom@gmail.com>
2022-06-22 10:58:56 +0100

    commit

---
+++ src/util/ClientId.js
@@ -0,0 +1,9 @@
+// jamming online wit google, elkom93@gmail.com
+// export const clientId = 'ce6b00ec8dbe4ffd8e73181548764f58';
+// export const redirectUri = 'https://spatulatom.github.io/jamming/';
+
+
+// // jamming local with google elkom93@gmail.com facebook account
+// export const clientId = '820f4b00f5ae47bc990077e132ea0c28';
+//  export const redirectUri = 'http://localhost:3000/';
+
